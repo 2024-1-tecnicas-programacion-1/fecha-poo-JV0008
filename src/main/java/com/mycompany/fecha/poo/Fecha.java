4a2e12c28811f8200c02ae3dc5fe2b7cf758678e
JV08 <josephd.vargaso@konradlorenz.edu.co>
2024-04-21 17:19:21 -0500

    Solución taller
    Se añadió lo necesario para solucionar lo propuesto en el taller.

--- src/main/java/com/mycompany/fecha/poo/Fecha.java
+++ src/main/java/com/mycompany/fecha/poo/Fecha.java
@@ -1,5 +1,49 @@
 package com.mycompany.fecha.poo;
 
+import java.util.HashMap;
 public class Fecha {
-    // TODO: Aquí va el código de la clase fechas
+int dia;
+int mes;
+int anio;
+    
+public Fecha (int dia, int mes, int anio) {
+this.dia = dia;
+this.mes = mes;
+this.anio = anio;
 }
+public String fechaCorta (){
+
+return (dia + "/" + mes + "/" + anio);
+}
+
+    public String mesLetra (int mes){
+HashMap<Integer, String> meses = new HashMap<>();
+meses.put(1, "enero");
+meses.put(2, "febrero");
+meses.put(3, "marzo");
+meses.put(4, "abril");
+meses.put(5, "mayo");
+meses.put(6, "junio");
+meses.put(7, "julio");
+meses.put(8, "agosto");
+meses.put(9, "septiembre");
+meses.put(10, "octubre");
+meses.put(11, "noviembre");
+meses.put(12, "diciembre");
+
+    String resultado = meses.get(mes);
+    return resultado;
+}
+        public boolean validarFecha (){
+    
+        return !(dia >= 32 || dia <= 0 || mes >= 13 || mes <= 0);   
+}
+    public String fechaLarga (){
+    if (validarFecha()){
+        String resultado = mesLetra(mes);
+        return (dia + " de " + resultado + " de " + anio);
+       }else 
+        return "";                              
+}
+}    
+
--- src/main/java/com/mycompany/fecha/poo/Principal.java
+++ src/main/java/com/mycompany/fecha/poo/Principal.java
@@ -1,5 +1,20 @@
 package com.mycompany.fecha.poo;
+import java.util.Scanner;
 
 public class Principal {
-    // TODO: Aquí va el código de la clase Principal
+public static void main(String[] args) {
+Scanner lect = new Scanner(System.in);
+int dia = Integer.parseInt(lect.nextLine());
+int mes = Integer.parseInt(lect.nextLine());
+int anio = Integer.parseInt(lect.nextLine());
+
+Fecha llamaDa = new Fecha(dia, mes, anio);
+    System.out.println(llamaDa.fechaCorta());
+    if (llamaDa.validarFecha()){
+        System.out.println("La fecha es válida");
+        System.out.println(llamaDa.fechaLarga());
+    }else {
+        System.out.println("Ingrese una fecha válida");
+    }
+}
 }
